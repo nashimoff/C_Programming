eada84670d21b1a815daccc538606cd82e392532
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-04-09 13:44:27 +0600

    sa

---
+++ Workspaces/Workspace2/OurFirstProgram/OurFirstProgram.mk
@@ -0,0 +1,114 @@
+##
+## Auto Generated makefile by CodeLite IDE
+## any manual changes will be erased      
+##
+## Debug
+ProjectName            :=OurFirstProgram
+ConfigurationName      :=Debug
+WorkspaceConfiguration :=Debug
+WorkspacePath          :=E:/COURSES/C_Programming/Workspaces/Workspace2
+ProjectPath            :=E:/COURSES/C_Programming/Workspaces/Workspace2/OurFirstProgram
+IntermediateDirectory  :=../build-$(WorkspaceConfiguration)/OurFirstProgram
+OutDir                 :=$(IntermediateDirectory)
+CurrentFileName        :=
+CurrentFilePath        :=
+CurrentFileFullPath    :=
+User                   :=n.ashimov
+Date                   :=09/04/2026
+CodeLitePath           :="E:/Program Files/CodeLite"
+MakeDirCommand         :=mkdir -p
+LinkerName             :=E:/cygwin64/bin/gcc.exe
+SharedObjectLinkerName :=E:/cygwin64/bin/gcc.exe -shared -fPIC
+ObjectSuffix           :=.o
+DependSuffix           :=.o.d
+PreprocessSuffix       :=.i
+DebugSwitch            :=-g 
+IncludeSwitch          :=-I
+LibrarySwitch          :=-l
+OutputSwitch           :=-o 
+LibraryPathSwitch      :=-L
+PreprocessorSwitch     :=-D
+SourceSwitch           :=-c 
+OutputDirectory        :=E:/COURSES/C_Programming/Workspaces/Workspace2/build-$(WorkspaceConfiguration)/bin
+OutputFile             :=..\build-$(WorkspaceConfiguration)\bin\$(ProjectName).exe
+Preprocessors          :=
+ObjectSwitch           :=-o 
+ArchiveOutputSwitch    := 
+PreprocessOnlySwitch   :=-E
+ObjectsFileList        :=$(IntermediateDirectory)/ObjectsList.txt
+PCHCompileFlags        :=
+RcCmpOptions           := 
+RcCompilerName         :=windres
+LinkOptions            :=  -static
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
+IncludePCH             := 
+RcIncludePath          := 
+Libs                   := 
+ArLibs                 :=  
+LibPath                := $(LibraryPathSwitch). 
+
+##
+## Common variables
+## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overridden using an environment variable
+##
+AR       := E:/cygwin64/bin/ar.exe
+CXX      := E:/cygwin64/bin/x86_64-pc-cygwin-g++.exe
+CC       := E:/cygwin64/bin/gcc.exe
+CXXFLAGS :=  -gdwarf-2 -O0 -Wall $(Preprocessors)
+CFLAGS   :=  -O0 -gdwarf-2 -std=c99 -Wall $(Preprocessors)
+ASFLAGS  := 
+AS       := E:/cygwin64/bin/as.exe
+
+
+##
+## User defined environment variables
+##
+CodeLiteDir:=E:\Program Files\CodeLite
+Objects0=$(IntermediateDirectory)/main.c$(ObjectSuffix) 
+
+
+
+Objects=$(Objects0) 
+
+##
+## Main Build Targets 
+##
+.PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
+all: MakeIntermediateDirs $(OutputFile)
+
+$(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
+	@$(MakeDirCommand) "$(IntermediateDirectory)"
+	@echo "" > $(IntermediateDirectory)/.d
+	@echo $(Objects0)  > $(ObjectsFileList)
+	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
+
+MakeIntermediateDirs:
+	@$(MakeDirCommand) "$(IntermediateDirectory)"
+	@$(MakeDirCommand) "$(OutputDirectory)"
+
+$(IntermediateDirectory)/.d:
+	@$(MakeDirCommand) "$(IntermediateDirectory)"
+
+PreBuild:
+
+
+##
+## Objects
+##
+$(IntermediateDirectory)/main.c$(ObjectSuffix): main.c $(IntermediateDirectory)/main.c$(DependSuffix)
+	$(CC) $(SourceSwitch) "E:/COURSES/C_Programming/Workspaces/Workspace2/OurFirstProgram/main.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.c$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/main.c$(DependSuffix): main.c
+	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.c$(ObjectSuffix) -MF$(IntermediateDirectory)/main.c$(DependSuffix) -MM main.c
+
+$(IntermediateDirectory)/main.c$(PreprocessSuffix): main.c
+	$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.c$(PreprocessSuffix) main.c
+
+
+-include $(IntermediateDirectory)/*$(DependSuffix)
+##
+## Clean
+##
+clean:
+	$(RM) -r $(IntermediateDirectory)
+
+
